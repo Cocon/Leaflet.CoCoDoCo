38e9a0cf7e45ddd230445d08825f39d60dd01fca
YUUKIToriyama <github@toriyama.dev>
2021-08-01 15:34:50 +0900

    add codes

--- src/CocoDoco.ts
+++ src/CocoDoco.ts
@@ -1,3 +1,27 @@
-import L, { extend } from 'leaflet';
+import L from 'leaflet';
+import 'leaflet-easybutton';
+import '@fortawesome/fontawesome-free';
 
-class CocoDoco extends L.Control { }
+export class CocoDoco {
+	map: L.Map;
+	constructor(mapInstance: L.Map, options?: L.ControlOptions) {
+		this.map = mapInstance;
+		L.easyButton({
+			position: options?.position || "topright",
+			leafletClasses: true,
+			states: [this.buttonEnabled, this.buttonUnabled]
+		}).addTo(this.map);
+	}
+	buttonEnabled: L.EasyButtonState = {
+		stateName: "enabled",
+		icon: "fa-location-dot",
+		title: "hogehoge",
+		onClick: () => { alert("hoge") }
+	}
+	buttonUnabled: L.EasyButtonState = {
+		stateName: "disabled",
+		icon: "fa-rotate-left",
+		title: "piyopiyo",
+		onClick: () => { alert("piyo") }
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { CocoDoco } from "./CocoDoco";
